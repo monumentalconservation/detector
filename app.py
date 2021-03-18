b310fec0b993d58d61a3f81ff970b3a1a3b38f43
Rosie-Brigham <rosiebrigham@gmail.com>
2021-03-18 22:29:43 +0000

    adds new endpoint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from model import get_prediction, batch_prediction
 from service_streamer import ThreadedStreamer
-
+from PIL import Image
+import urllib.request
 
 app = Flask(__name__)
 streamer = ThreadedStreamer(batch_prediction, batch_size=64)
@@ -27,6 +28,17 @@ def stream_predict():
         file = request.files['file']
         img_bytes = file.read()
         class_id, class_name = streamer.predict([img_bytes])[0]
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+
+
+@app.route('/stream_url_predict', methods=['POST'])
+def stream_url_predict():
+    print('Stream predicting from url in progress!!')
+    if request.method == 'POST':
+        file_url = request.form.get('file_url')
+        im = urllib.request.urlopen(file_url).read()
+
+        class_id, class_name = streamer.predict([im])[0]
         return jsonify({'class_id': class_id, 'class_name': class_name})
 
 
--- readme.md
+++ readme.md
@@ -4,5 +4,7 @@
 # test endpoint
 `curl -F "file=@machrie-1.jpg" http://localhost:5000/stream_predict`
 
+`curl --data "file_url=https://www.monumentmonitor.co.uk/static/media/machrie.602f34b9.jpg" -X POST http://localhost:5000/stream_url_predict`
+
 # test production endpoint
-`curl -F "file=@machrie-1.jpg" http://localhost:5000/stream_predict`
+`curl -F "file=@machrie-1.jpg" https://mmdetector.herokuapp.com/stream_predict`
